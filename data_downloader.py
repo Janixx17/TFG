115ecb2a1f72081a7dbe1904d0696d8d50b558c1
jansh <janshadow17@gmail.com>
2025-03-24 18:33:46 +0100

    Script to download data (dev still)

--- data_downloader.py
+++ data_downloader.py
@@ -1,10 +1,8 @@
 from os import write
 import yfinance as yf
 import pandas as pd
-
-def download_data(ticker, start, end):
-    data = yf.download(ticker, start=start, end=end)
-    return data
+import datetime as dt
+import numpy as np
 
 def get_simbols():
     link = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies#S&P_500_component_stocks'
@@ -39,12 +37,42 @@ def get_top_100_index():
 
         writer.writerows(sptop100)
 
+def get_data(Symbols, times): #TODO guardar-la
+    for symbol in Symbols:
+        for time in times:
+            #if time[:, 1] == 8:#todo
+            #    continue
+            print(symbol, time)
+            day1, day2 = calculate_time(int(time[1])) #time must be numpy array
+            if time[0] != 7299:
+                data = yf.download(symbol, end=day1.today(), start=day2, interval=f"{time[0]}m")
+                data.to_csv(f"data/{symbol}_{day1}_{day2}_{time[0]}m.csv")
+            else:
+                data = yf.download(symbol, end=day1, start=day2, interval="1d")
+                data.to_csv(f"data/{symbol}_{day1}_{day2}_1d.csv")
+
+
+
+def calculate_time(time):
+    return dt.date.today(), dt.date.today() - dt.timedelta(days=time)
+
+
 if __name__ == '__main__':
 
-    get_top_100_index()
+    #get_top_100_index()
 
-    data = yf.download("NVDA", start="2023-01-01", end="2024-03-01", interval="1h")
-    print(data.head())
+    symbols = np.loadtxt("top100.csv", delimiter=",", dtype=str, skiprows=1)
+    symbols = symbols[:, 0]
+    times = np.loadtxt("times.csv", delimiter=",", dtype=int, skiprows=1)
+
+    auxsymbols = symbols[0:2]
+    auxtimes = times[1:3]
+
+    get_data(auxsymbols, auxtimes)
+
+
+    #data = yf.download("NVDA", start="2002-01-01", end="2025-03-23", interval="1h")
+    #print(data.head())
 
 
 
